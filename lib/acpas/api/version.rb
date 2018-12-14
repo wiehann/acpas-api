51538e333378623d47af924846b81c80eb4652f8
wiehann <wradyn@gmail.com>
2018-12-14 08:34:35 +0200

    Updates Rack for CVE-2018-16471

--- lib/acpas/api/version.rb
+++ lib/acpas/api/version.rb
@@ -3,7 +3,7 @@ module Acpas
     module VERSION
       MAJOR = 0
       MINOR = 0
-      TINY  = 4
+      TINY  = 5
       PRE   = nil
       STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
     end
